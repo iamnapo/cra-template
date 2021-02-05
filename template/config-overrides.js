5e6ac5ce94abbfbeee2d341f185e66b1b52b6b06
Napoleon Oikonomou <napoleonoikon@gmail.com>
2021-02-05 14:27:52 +0200

    Use new `DISABLE_ESLINT_PLUGIN`, Update deps

--- template/config-overrides.js
+++ template/config-overrides.js
@@ -4,8 +4,4 @@ module.exports = override(
 	fixBabelImports("@material-ui/core", { libraryDirectory: ".", camel2DashComponentName: false }),
 	fixBabelImports("@material-ui/icons", { libraryDirectory: ".", camel2DashComponentName: false }),
 	fixBabelImports("@material-ui/lab", { libraryDirectory: ".", camel2DashComponentName: false }),
-	(config) => {
-		config.plugins = config.plugins.filter((e) => !e.options?.eslintPath);
-		return config;
-	},
 );
